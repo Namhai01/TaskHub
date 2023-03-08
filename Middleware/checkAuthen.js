67d97d9c024c630b466040ecbcc988b89dff4436
Namhai01 <dgn3072000@gmail.com>
2023-03-08 16:29:30 +0700

    update midelware

--- Middleware/checkAuthen.js
+++ Middleware/checkAuthen.js
@@ -1,6 +1,5 @@
 function checkAuthentication (req , res, next) {
     if(req.isAuthenticated()){
-        return res.json({status: 'Susscess', message: 'Login successful'});
         next();
     }else{
        res.json({status: 'Error', message: 'NotAuthenticated'})
